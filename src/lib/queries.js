c5a78962d59655b06d12e6ff02147d5c884b1440
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-08-04 12:35:44 +0200

    settingsQuery added

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,19 +1,25 @@
 import { groq } from 'next-sanity'
 
 export const pagesPathQuery = groq`
-*[_type == "page" && defined(slug.current)][] {
+*[_type == 'page' && defined(slug.current)][] {
     'slug': slug.current,
 }
 `
 
 export const pageQuery = groq`
-*[_type == "page" && slug.current == $slug][0] {
+*[_type == 'page' && slug.current == $slug][0] {
     ...,
 }
 `
 
 export const frontPageQuery = groq`
-*[_type == "frontpage"][0] {
+*[_type == 'frontpage'][0] {
+    ...,
+}
+`
+
+export const settingsQuery = groq`
+*[_type == 'settings'][0] {
     ...,
 }
 `
