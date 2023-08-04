4d72213e74e90a7305e6617322029d8913c6977e
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-08-04 12:27:23 +0200

    settings and components added

--- src/sanity/schemas/index.js
+++ src/sanity/schemas/index.js
@@ -1,10 +1,13 @@
 import page from './documents/page'
 // Singletons
 import frontpage from './singletons/frontpage'
+import settings from './singletons/settings'
 // Objects
 import mainImage from './objects/mainImage'
 import seo from './objects/seo'
 import portableText from './objects/portableText'
+import linkInternal from './objects/linkInternal'
+import linkExternal from './objects/linkExternal'
 // Content-blocks
 import hero from './content-blocks/hero'
 import textBlock from './content-blocks/textBlock'
@@ -14,10 +17,13 @@ export const schemaTypes = [
   page,
   // Singletons
   frontpage,
+  settings,
   // Objects
   mainImage,
   seo,
   portableText,
+  linkInternal,
+  linkExternal,
   // Content-blocks
   hero,
   textBlock,
