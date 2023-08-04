9f9320a890a9ee421a22c88baca28bc85a680edd
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-08-04 12:35:00 +0200

    init

---
+++ src/lib/getMetaObject.js
@@ -0,0 +1,16 @@
+import { getCachedClient } from './getClient'
+import { settingsQuery } from './queries'
+
+export default async function getMetaObject(meta) {
+  // Get default meta from settings if seo not complete
+  let defaultMeta = null
+  if (!meta?.title || meta?.description) {
+    const settings = await getCachedClient()(settingsQuery)
+    defaultMeta = settings.defaultSeo
+  }
+
+  return {
+    title: meta?.title ?? defaultMeta?.title,
+    description: meta?.description ?? defaultMeta?.description,
+  }
+}
