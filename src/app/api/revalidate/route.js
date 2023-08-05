1271983fa5bf9ea6cd4b336db9b6d3a51d7ae9c1
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-08-05 12:32:11 +0200

    API test

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -2,30 +2,13 @@ import { NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
 import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 
-// import { parseBody } from 'next-sanity/webhook'
-// export { config } from 'next-sanity/webhook'
-
 const secret = process.env.REVALIDATION_TOKEN
 
 export async function POST(req) {
   const signature = req.headers.get(SIGNATURE_HEADER_NAME)
   const body = await req.json()
-  // console.log(body)
-  const bodyString = JSON.stringify(body)
 
-  // const { isValidSignature, body } = await parseBody(req, secret)
-
-  const isValid = isValidSignature(bodyString, signature, secret)
-
-  const message = isValid
-  console.log(message)
-  return NextResponse.json({ success: false, message }, { status: 401 })
-
-  console.log('######')
-  console.log(body)
-  console.log('######')
-
-  if (!isValidSignature) {
+  if (!isValidSignature(JSON.stringify(body), signature, secret)) {
     const message = 'Invalid signature'
     console.log(message)
     return NextResponse.json({ success: false, message }, { status: 401 })
@@ -34,7 +17,7 @@ export async function POST(req) {
   const path = resolvePath(body)
 
   if (!path) {
-    const message = 'No path provided'
+    const message = 'Could not resolve path'
     console.log(message)
     return NextResponse.json({ success: false, message }, { status: 400 })
   }
@@ -43,8 +26,9 @@ export async function POST(req) {
 
   try {
     revalidatePath(path)
-    console.log(`Revalidated: '${path}'`)
-    return NextResponse.json({ success: true }, { status: 200 })
+    const message = `Revalidated: '${path}'`
+    console.log(message)
+    return NextResponse.json({ success: true, message }, { status: 200 })
   } catch (error) {
     console.log(error)
     return NextResponse.json({ success: false, message: 'Revalidation failed' }, { status: 500 })
@@ -52,8 +36,9 @@ export async function POST(req) {
 }
 
 function resolvePath(body) {
-  if (!body?.slug?.current) return '/'
-  return `/${body.slug.current}`
+  if (!body?._type == 'frontpage') return '/' // Frontpage
+  else if (body?.slug?.current) return `/${body.slug.current}` // Pages
+  return undefined
 }
 
 // Sleep API - May be needed to make sure DB is updated before revalidating
