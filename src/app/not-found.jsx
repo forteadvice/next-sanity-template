1614dd684dfdc880c2361b6e233a2d943e86685f
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-08-07 12:50:15 +0200

    404 data from settings

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,14 +5,17 @@ import Footer from '@/components/Footer'
 
 export default async function NotFound() {
   const settings = await getCachedClient()(settingsQuery)
+  const { menu, pageNotFound, footer } = settings
 
   return (
     <div className='h-screen flex flex-col'>
-      <Menu data={settings?.menu} />
+      <Menu data={menu} />
       <main className='flex-1 flex justify-center items-center'>
-        <h1 className='text-2xl mb-2'>404 | Page not found</h1>
+        <h1 className='text-2xl mb-2'>
+          {pageNotFound?.title} | {pageNotFound?.body}
+        </h1>
       </main>
-      <Footer data={settings?.footer} />
+      <Footer data={footer} />
     </div>
   )
 }
