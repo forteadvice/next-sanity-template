c26d8d19faa1cdf2cdbe68fc5c0efc55dcfd2505
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-11-30 09:44:33 +0100

    layouts rearranged

--- src/app/(site)/layout.jsx
+++ src/app/(site)/layout.jsx
@@ -5,16 +5,20 @@ import Footer from '@/components/Footer'
 import getPreview from '@/lib/getPreview'
 import ExitPreview from '@/components/preview/ExitPreview'
 
+import '../../styles/globals.css'
+
 export default async function BaseLayout({ children }) {
   const preview = getPreview()
   const settings = await sanityFetch({ query: settingsQuery, tags: ['settings'] })
 
   return (
-    <>
-      <Menu data={settings?.menu} />
-      {children}
-      {preview && <ExitPreview />}
-      <Footer data={settings?.footer} />
-    </>
+    <html lang='en'>
+      <body>
+        <Menu data={settings?.menu} />
+        {children}
+        {preview && <ExitPreview />}
+        <Footer data={settings?.footer} />
+      </body>
+    </html>
   )
 }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,3 @@
-import '../styles/globals.css'
-
-export default async function BaseLayout({ children }) {
-  return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }) {
+  return <>{children}</>
 }
