6a900cdf3271ccc0b7ccc0a2064a992f5442bc4b
Emil Nakayama <senakster@gmail.com>
2023-12-15 13:29:10 +0100

    rm import

--- src/components/globals/MainContentLink.jsx
+++ src/components/globals/MainContentLink.jsx
@@ -1,5 +1,3 @@
-import { Link } from 'next/link';
-
 export default function MainContentLink() {
     return (
             <a href="#main" className="absolute opacity-0 -top-full -left-full focus-visible:top-10 focus-visible:left-10 focus-visible:opacity-100">
