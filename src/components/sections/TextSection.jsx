d05b5555a528ef3b50315308f8efdfdfd54409ce
Emil Nakayama <senakster@gmail.com>
2023-12-20 13:29:42 +0100

    init

--- src/components/sections/TextSection.jsx
+++ src/components/sections/TextSection.jsx
@@ -3,24 +3,56 @@ import { PortableText } from '@portabletext/react'
 
 export default function TextSection({ data, mt = true, mb = true }) {
   return (
-    <div className={`text-section ${mt ? typeof mt === 'string' ? mt : 'mt-8' : ''} ${mb ? typeof mb === 'string' ? mb : 'mt-8' : ''}`}>
+    <div className={`${mt ? typeof mt === 'string' ? mt : 'mt-8' : ''} ${mb ? typeof mb === 'string' ? mb : 'mb-8' : ''}`}>
       <PortableText value={data?.text} components={components} />
     </div>
   )
 }
 
+
 // Custom components config
+const defaultMargins = 'mb-2 last:mb-0'
 const components = {
+  block: {
+    h2: props => (
+      <h2 className={`text-4xl font-bold ${defaultMargins}`}>{props?.children}</h2>
+    ),
+    h3: props => (
+      <h3 className={`text-3xl font-semibold ${defaultMargins}`}>{props?.children}</h3>
+    ),
+    h4: props => (
+      <h4 className={`text-2xl font-semibold ${defaultMargins}`}>{props?.children}</h4>
+    ),
+    blockquote: props => (
+      <blockquote className={`italic before:content-['“'] after:content-['”'] ${defaultMargins}`}>{props?.children}</blockquote>
+    ),
+    normal: props =>{
+      return (
+      <p className={`${defaultMargins}`}>{props?.children}</p>
+    )},
+  },
   marks: {
     internalLink: InternalLink,
     externalLink: ExternalLink,
   },
+  list: {
+    bullet: unorderedList,
+    number: orderedList,
+}
 }
 
-// Custom components design
+// Custom components config
+function unorderedList({children}) { 
+  return (<ul className={`list-disc list-inside ${defaultMargins}`}>{children}</ul>)
+}
+
+function orderedList({children}) {
+return (<ol className={`list-decimal list-inside ${defaultMargins}`}>{children}</ol>)
+}
+
 function InternalLink(props) {
   return (
-    <Link href={`/${props?.value?.path}`} prefetch={false}>
+    <Link className="underline" href={`/${props?.value?.path}`} prefetch={false}>
       {props.text}
     </Link>
   )
@@ -28,7 +60,7 @@ function InternalLink(props) {
 
 function ExternalLink(props) {
   return (
-    <a target={'_blank'} href={resolveUrl(props?.value?.href)}>
+    <a className="underline" target={'_blank'} href={resolveUrl(props?.value?.href)}>
       {props.children}
     </a>
   )
