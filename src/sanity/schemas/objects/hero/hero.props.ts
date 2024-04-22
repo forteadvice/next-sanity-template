9889a82f862aa471f31e9a4630f5043a75cee3c9
Kasper Filstrup <kasperfilstrup@gmail.com>
2024-04-22 12:50:30 +0200

    Fix build errors after rebase

--- src/sanity/schemas/objects/hero/hero.props.ts
+++ src/sanity/schemas/objects/hero/hero.props.ts
@@ -1,7 +1,7 @@
 import { type TBaseImage } from '../../fields/baseImage/baseImage.props'
 
 export type THero = {
-  title?: string
-  tagline?: string
+  headline?: string
+  manchet?: string
   image?: TBaseImage
 }
--- src/sanity/schemas/objects/hero/hero.query.ts
+++ src/sanity/schemas/objects/hero/hero.query.ts
@@ -2,7 +2,7 @@ import { groq } from 'next-sanity'
 import { baseImageQuery } from '../../fields/baseImage/baseImage.query'
 
 export const heroQuery = groq`{
-  title,
-  tagline,
+  headline,
+  manchet,
   image ${baseImageQuery}
 }`
