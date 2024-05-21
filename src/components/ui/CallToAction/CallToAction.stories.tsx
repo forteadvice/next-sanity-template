816a9897ce869c4b9c00d0f162bf14246a5a2a33
Kasper Filstrup <632686+kasperfilstrup@users.noreply.github.com>
2024-05-21 13:33:13 +0200

    Components/call to action (#34)
    * Initial draft of CallToAction (bgcolor / image / video super hero)
    
    * Wrap up CTA Super Hero

---
+++ src/components/ui/CallToAction/CallToAction.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import CallToAction from '.'
+import imageFile from './image.jpg'
+import videoFile from './video.mp4'
+console.log(videoFile)
+import Button from '../Button'
+
+const meta: Meta<typeof CallToAction> = {
+  title: 'UI/CallToAction',
+  component: CallToAction,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+  args: {
+    image: imageFile.src,
+    video: videoFile,
+    heading: "I'm a bold heading",
+    subHeading: 'Sub sub what up',
+    button: <Button href='#'>Click here</Button>,
+    body: (
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A magni placeat velit impedit qui
+        expedita beatae consequatur!
+        <br />
+        Perferendis natus sit nostrum, porro deleniti sequi cupiditate eaque veritatis
+        exercitationem ipsum dolorem.
+      </p>
+    ),
+  },
+}
+export default meta
+
+type Story = StoryObj<typeof CallToAction>
+
+export const Primary: Story = {}
